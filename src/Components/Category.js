0f0f5e4482ecf5e548706f7c28997261fbafb964
Anusha <47149499+anushanagula@users.noreply.github.com>
2022-02-05 19:58:28 +0530

    Update Category.js

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -7,7 +7,9 @@ class Category extends React.Component
     constructor(props) {
         super(props)
         //this.handleOnChange = this.handleOnChange.bind(this);
-        this.state = {category1:''}
+        this.state = {category1:'',
+                      category2:'',
+                      category3:'',}
 
       }
       getUniqueCategoryName(products) {
@@ -16,12 +18,52 @@ class Category extends React.Component
           <option key={index}>{category1}</option>
         ));
       }
+      getUniqueCategoryName2(items,category1) {
+        if(category1)
+        {
+        const products=items.data.filter(function(item){
+          return item.category_level1 == category1;         
+              })
+        const category = [... new Set(products.map((item)=> {return item.category_level2}))]
+        return category.map((category2, index) => (
+          <option key={index}>{category2}</option>
+        ));
+        }
+        else 
+        return 
+      }
+      getUniqueCategoryName3(items,category2) {
+        if(category2)
+        {
+        const products=items.data.filter(function(item){
+          return item.category_level2 == category2;         
+              })
+        const category = [... new Set(products.map((item)=> {return item.category_level3}))]
+        return category.map((category3, index) => (
+          <option key={index}>{category3}</option>
+        ));
+        }
+        else 
+        return 
+      }
       handleOnChange=(e)=>{
+        e.preventDefault()
         this.setState({ category1: e.target.value })
+
+      }
+      handleOnChange2=(e)=>{
+        e.preventDefault()
+        this.setState({ category2: e.target.value })
+
+      }
+      handleOnChange3=(e)=>{
+        e.preventDefault()
+        this.setState({ category3: e.target.value })
+
       }
       handleOnSubmit=(e)=>{
           e.preventDefault()
-        this.props.history.push({pathname:'/search',state:{category1: this.state.category1}});
+        this.props.history.push({pathname:'/search',state:{category1: this.state.category1,category2:this.state.category2,category3:this.state.category3}});
       }
     render()
     {
@@ -41,16 +83,20 @@ class Category extends React.Component
                     {this.getUniqueCategoryName(items)}
                     </select>
                     <label>Select Category2 :</label>
-                  <select>
+                  <select  onChange={this.handleOnChange2.bind(this)}>  
                     <option>Select a Category</option>
-                    </select>
+                    {this.state.category1}?(
+                    {this.getUniqueCategoryName2(items,this.state.category1)}):()
+                    </select>    
                     <label>Select Category3 :</label>
-                  <select>
+                  <select  onChange={this.handleOnChange2.bind(this)}>
                     <option>Select a Category</option>
+                    {this.state.category2}?(
+                    {this.getUniqueCategoryName3(items,this.state.category2)}):()
                     </select>
-                <button onClick={this.handleOnSubmit}>Submit</button>
               </div>
               </div>
+              <button onClick={this.handleOnSubmit}>Submit</button>
           </div>               
         
         
